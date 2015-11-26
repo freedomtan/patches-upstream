6fc4aba33e4f47910bc0a4ce85c669626708f14d
freedom.tan <freedom.tan@mediatek.com>
2015-11-26 13:40:43 +0800

    fixed schedtune cgroup problem and others
    1. for-ipa:  add dts entries and IPA callbacks
    2. other-WIP-patches: other WIP EAS patches from Chromebook patches
    3. some-tracepints: tracepoints for tracking PELT

--- patch-eas.sh
+++ patch-eas.sh
@@ -1,3 +1,3 @@
-for p in eas-v51-sched-dvfs schedtune migration-hacks; do 
+for p in eas-v51-sched-dvfs schedtune migration-hacks some-tracepoints other-WIP-patches for-ipa; do
 	git am ../patches-upstream/$p/*.patch; 
 done
