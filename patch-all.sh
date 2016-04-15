f1cfd0c59d1ce1870abce4c080e6f8e9948c7513
Koan-Sin Tan <koansin.tan@gmail.com>
2016-04-15 15:14:09 +0800

    for v4.6-rc3

--- patch-all.sh
+++ patch-all.sh
@@ -1,3 +1,3 @@
-for p in mfd clk psci-and-cpuidle cpufreq others thermal mem; do 
+for p in scpsys mfd clk psci-and-cpuidle cpufreq others thermal mem; do
 	git am ../patches-upstream/$p/*.patch; 
 done
