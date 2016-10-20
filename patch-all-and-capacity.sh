fc76f19516ee659cffa476f762e6beb4efa7641a
Koan-Sin Tan <freedom.tan@mediatek.com>
2016-10-20 10:53:16 +0800

    for 4.9-rc1
    tested on v4.9-rc1
    
    Updated capacity-awareness and eas related patches with MT8173 settings
    Patches from [1]
        capacity-v4: from branch capacity_awareness_v5_arm64_v2
        eas-20161014: from branch eas/next/integration_20161014_1237
    
        [1] http://www.linux-arm.org/git?p=linux-power.git;a=summary

--- patch-all-and-capacity.sh
+++ patch-all-and-capacity.sh
@@ -1,3 +1,3 @@
-for p in clk psci-and-cpuidle cpufreq others thermal mem some-more-crbook-patches cci capacity-v4; do
+for p in clk psci-and-cpuidle cpufreq others thermal mem some-more-crbook-patches cci capacity-v5; do
 	git am ../patches-upstream/$p/*.patch; 
 done
--- patch-all-and-eas.sh
+++ patch-all-and-eas.sh
@@ -1,3 +1,3 @@
-for p in clk psci-and-cpuidle cpufreq others thermal mem some-more-crbook-patches cci eas-20160902; do
+for p in clk psci-and-cpuidle cpufreq others thermal mem some-more-crbook-patches cci eas-20161014; do
 	git am ../patches-upstream/$p/*.patch; 
 done
