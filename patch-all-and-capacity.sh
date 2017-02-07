0ed63afa3166319f65de9c0594e3aa418d2520f6
Koan-Sin Tan <koansin.tan@gmail.com>
2017-02-07 16:29:19 +0800

    patches from SoC idle
    genpd: genpd related and other patches from lina's repo

--- patch-all-and-capacity.sh
+++
@@ -1,3 +0,0 @@
-for p in clk psci-and-cpuidle cpufreq others thermal mem some-more-crbook-patches cci capacity-v5; do
-	git am ../patches-upstream/$p/*.patch; 
-done
--- patch-all-and-eas.sh
+++
@@ -1,3 +0,0 @@
-for p in clk psci-and-cpuidle cpufreq others thermal mem some-more-crbook-patches cci eas-20161014; do
-	git am ../patches-upstream/$p/*.patch; 
-done
